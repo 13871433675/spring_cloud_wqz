d8af225960ec7a15a60b2ef84e799636d2b48a3b
qizhi.wang <qizhi.wang@rograndec.com>
2019-06-14 15:55:38 +0800

    hystrix dashboard in ribbon commit

--- eureka-ribbon-test/src/main/java/eurekaribbon/eurekaribbontest/EurekaRibbonTestApplication.java
+++ eureka-ribbon-test/src/main/java/eurekaribbon/eurekaribbontest/EurekaRibbonTestApplication.java
@@ -6,12 +6,14 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableHystrix
+@EnableHystrixDashboard
 public class EurekaRibbonTestApplication {
 
 	public static void main(String[] args) {
--- eureka-ribbon-test/src/main/java/eurekaribbon/eurekaribbontest/HelloService.java
+++ eureka-ribbon-test/src/main/java/eurekaribbon/eurekaribbontest/HelloService.java
@@ -9,7 +9,6 @@ import org.springframework.web.client.RestTemplate;
 public class HelloService {
     @Autowired
     RestTemplate restTemplate;
-
     @HystrixCommand(fallbackMethod = "hiError")
     public String hiService(String name){
         return restTemplate.getForObject("http://SERVICE-SAYHI/hi?name="+name,String.class);
