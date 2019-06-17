e8bf2166c17d3c19c43f06e3afa504a51d6dceba
qizhi.wang <qizhi.wang@rograndec.com>
2019-06-17 17:57:52 +0800

    ribbon 自定义负载均衡代码提交

--- eureka-ribbon-test/src/main/java/eurekaribbon/eurekaribbontest/test/MyLoadBalancerAutoConfiguration.java
+++ eureka-ribbon-test/src/main/java/eurekaribbon/eurekaribbontest/test/MyLoadBalancerAutoConfiguration.java
@@ -1,17 +1,17 @@
 package eurekaribbon.eurekaribbontest.test;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+        import java.util.ArrayList;
+        import java.util.Collections;
+        import java.util.List;
 
-import org.springframework.beans.factory.ObjectProvider;
-import org.springframework.beans.factory.SmartInitializingSingleton;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.loadbalancer.RestTemplateCustomizer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.ClientHttpRequestInterceptor;
-import org.springframework.web.client.RestTemplate;
+        import org.springframework.beans.factory.ObjectProvider;
+        import org.springframework.beans.factory.SmartInitializingSingleton;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.cloud.client.loadbalancer.RestTemplateCustomizer;
+        import org.springframework.context.annotation.Bean;
+        import org.springframework.context.annotation.Configuration;
+        import org.springframework.http.client.ClientHttpRequestInterceptor;
+        import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class MyLoadBalancerAutoConfiguration {
@@ -20,28 +20,28 @@ public class MyLoadBalancerAutoConfiguration {
     private List<RestTemplate> restTemplates = Collections.emptyList();
 
     @Bean
-    public MyLoadBalancerinterceptor myLoadBalancerInterceptornterceptor ( ) {
+    public MyLoadBalancerinterceptor myLoadBalancerInterceptornterceptor() {
         return new MyLoadBalancerinterceptor();
     }
 
     @Bean
-    public SmartInitializingSingleton myLoadBalancedRestTemplateinitializer(final ObjectProvider<List<RestTemplateCustomizer>> restTemplateCustomizers){
+    public SmartInitializingSingleton myLoadBalancedRestTemplateinitializer(final ObjectProvider<List<RestTemplateCustomizer>> restTemplateCustomizers) {
 //        return  () -> restTemplateCustomizers.ifAvailable(customizers -> {
 //            for (RestTemplate restTemplate : MyLoadBalancerAutoConfiguration.this.restTemplates)
 //                for (RestTemplateCustomizer customizer : customizers) {
 //                    customizer.customize(restTemplate);
 //                }
 //        });
-            return  new SmartInitializingSingleton() {
-                @Override
-                public void afterSingletonsInstantiated() {
-                    for (RestTemplate restTemplate : MyLoadBalancerAutoConfiguration.this.restTemplates) {
-                        List<ClientHttpRequestInterceptor> list = new
-                                ArrayList<>(restTemplate.getInterceptors());
-                        list.add(myLoadBalancerInterceptornterceptor());
-                        restTemplate.setInterceptors(list);
-                    }
+        return new SmartInitializingSingleton() {
+            @Override
+            public void afterSingletonsInstantiated() {
+                for (RestTemplate restTemplate : MyLoadBalancerAutoConfiguration.this.restTemplates) {
+                    List<ClientHttpRequestInterceptor> list = new
+                            ArrayList<>(restTemplate.getInterceptors());
+                    list.add(myLoadBalancerInterceptornterceptor());
+                    restTemplate.setInterceptors(list);
                 }
-            };
+            }
+        };
     }
 }
